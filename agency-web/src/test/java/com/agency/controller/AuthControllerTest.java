32cb3e6dcc71be31c76793ea340818e855eec1df
arturszmer <arturszmer@gmail.com>
2024-04-18 13:44:34 +0200

    add integration tests to block and admin initializer endpoints

--- agency-web/src/test/java/com/agency/controller/AuthControllerTest.java
+++ agency-web/src/test/java/com/agency/controller/AuthControllerTest.java
@@ -1,21 +1,31 @@
 package com.agency.controller;
 
 import com.agency.BaseIntegrationTestSettings;
+import com.agency.auth.AdminInitializerDto;
 import com.agency.auth.AuthenticationResponse;
 import com.agency.auth.RegistrationRequest;
 import com.agency.auth.RoleType;
+import com.agency.user.model.Role;
+import com.agency.user.model.UserProfile;
+import com.agency.user.repository.UserProfileRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class AuthControllerTest extends BaseIntegrationTestSettings {
+
+    @Autowired
+    private UserProfileRepository repository;
 
     @Test
     public void shouldReturn401WhenUserIsNotAuthenticated() throws Exception {
@@ -26,9 +36,38 @@ class AuthControllerTest extends BaseIntegrationTestSettings {
     }
 
     @Test
-    public void shouldRegisterNewUser() throws Exception {
+    public void should_create_admin_account() throws Exception {
+        String username = "agency-admin";
+        AdminInitializerDto request = new AdminInitializerDto(username, "admin@admin.pl", "password");
+        String body = mapper.writeValueAsString(request);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/auth/admin-initializer")
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        AuthenticationResponse authenticationResponse = mapper.readValue(mvcResult.getResponse().getContentAsString(), AuthenticationResponse.class);
+
+        // then
+        assertNotNull(authenticationResponse.accessToken());
+        assertEquals(200, mvcResult.getResponse().getStatus());
+
+        Optional<UserProfile> userProfileByUsername = repository.findUserProfileByUsername(username);
+        assertTrue(userProfileByUsername.isPresent());
+        RoleType roleType = userProfileByUsername.get().getRoles().stream()
+                .filter(role -> RoleType.ADMIN == role.getName())
+                .findFirst()
+                .map(Role::getName)
+                .orElseThrow();
+        assertEquals(RoleType.ADMIN, roleType);
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_ADMIN")
+    public void should_register_new_user() throws Exception {
         // given
-        RegistrationRequest request = new RegistrationRequest("user-auth-controller", "user@user.pl", "password", RoleType.USER);
+        String username = "user-auth-controller";
+        RegistrationRequest request = new RegistrationRequest(username, "user@user.pl", "password", RoleType.USER);
         String body = mapper.writeValueAsString(request);
 
         // when
@@ -42,5 +81,6 @@ class AuthControllerTest extends BaseIntegrationTestSettings {
         // then
         assertNotNull(authenticationResponse.accessToken());
         assertEquals(200, mvcResult.getResponse().getStatus());
+        assertTrue(repository.findUserProfileByUsername(username).isPresent());
     }
 }
--- agency-web/src/test/resources/sql-init/user-profile-init.sql
+++ agency-web/src/test/resources/sql-init/user-profile-init.sql
@@ -1,7 +1,17 @@
 INSERT INTO user_profile (
     creation_timestamp, modification_timestamp,
-                        created_by, email, first_name, last_modified, last_name, password, username
-) VALUES (
+                        created_by, email, first_name, last_modified, last_name, password, username, account_non_locked)
+VALUES (
     CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
-    'admin', 'admin@example.com', 'Admin', CURRENT_TIMESTAMP, 'Admin', 'password', 'admin'
-    );
+    'admin', 'admin@example.com', 'Admin', CURRENT_TIMESTAMP, 'Admin', 'password', 'admin', true);
+
+
+INSERT INTO user_profile (creation_timestamp, modification_timestamp,
+    created_by, email, first_name, last_modified, last_name, password, username, account_non_locked)
+VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
+        'admin', 'test@example.com', 'userTest', CURRENT_TIMESTAMP, 'user', 'password', 'userTest', true);
+
+INSERT INTO user_profile (creation_timestamp, modification_timestamp,
+                          created_by, email, first_name, last_modified, last_name, password, username, account_non_locked)
+VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
+        'admin', 'testLocked@example.com', 'userTestLocked', CURRENT_TIMESTAMP, 'user', 'password', 'userTestLocked', false);
