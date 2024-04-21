b175219cf4df43a29e9185deda9789d0b41807ac
arturszmer <arturszmer@gmail.com>
2024-04-21 15:33:46 +0200

    improve test

--- agency-web/src/test/java/com/agency/controller/agency/AgencyDetailsControllerTest.java
+++ agency-web/src/test/java/com/agency/controller/agency/AgencyDetailsControllerTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Transactional
 @WithMockUser(roles = "ADMIN")
 class AgencyDetailsControllerTest extends BaseIntegrationTestSettings {
 
